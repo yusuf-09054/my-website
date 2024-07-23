9874c2d42c08786fd3ab6c6803e5df55364d69c8
yusuf-09054 <smohammedyusuf193@gmail.com>
2024-07-23 22:46:50 +0530

    done1

--- mail.js
+++ mail.js
@@ -1,41 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 
-const app = express();
-const PORT = 3000;
+function sendMail(){
+    var params1 = {
+        name: document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
 
-app.use(bodyParser.json());
-
-app.post('/send-email', (req, res) => {
-    const { userEmail, subject, message } = req.body;
-
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'smohammedyusuf193@gmail.com',
-            pass: 'Yusuf 005'
-        }
-    });
-
-    let mailOptions = {
-        from: 'your_email@gmail.com',
-        to: 'smohammedyusuf193@gmail.com', // Replace with your own email address
-        subject: subject,
-        text: `From: ${userEmail}\n\n${message}`
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-            res.status(500).send('Failed to send email');
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.status(200).send('Email sent successfully');
-        }
-    });
-});
+const serviceID = "service_cu7a1uf";
+const templateID = "template_0hgtz7n"
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+emailjs.send(serviceID,templateID,params1)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message is sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
