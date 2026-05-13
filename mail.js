df7dc6dbb7b66981afe1100d71391b297696f894
Yusuf-09054 <yusufmca9@gmail.com>
2026-05-13 18:23:44 +0530

    first commit

---
+++ mail.js
@@ -0,0 +1,23 @@
+
+function sendMail(){
+    var params1 = {
+        name: document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+
+    };
+
+const serviceID = "service_ckuwcqd";
+const templateID = "template_0hgtz7n"
+
+emailjs.send(serviceID,templateID,params1)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message is sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
